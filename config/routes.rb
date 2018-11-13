45d3f72edbac5c6c36c8711cd287a5a0424a198c
Wiebke Frey <frey.wiebke@googlemail.com>
2018-11-13 14:11:13 +0100

    Add task list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # # show all tasks
+  # get 'tasks', to: 'tasks#index', as: 'tasks'
+
+  # # create a new tasks
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
+
+  # # show details of one task
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # # update a task
+  # get 'task/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
